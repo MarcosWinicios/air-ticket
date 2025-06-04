b11d691c124fbfdc62863828c7292fae50fe6af7
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2025-06-04 09:16:40 -0300

    feat(service): Create ticket price service class

---
+++ src/main/java/com/studies/service/TickerPriceService.java
@@ -0,0 +1,10 @@
+package com.studies.service;
+
+import com.studies.Flight;
+import com.studies.Passenger;
+
+public class TickerPriceService {
+    public double calculate(Passenger passenger, Flight flight) {
+        return 0;
+    }
+}
