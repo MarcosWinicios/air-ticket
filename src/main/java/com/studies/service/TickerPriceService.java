0aaa45dbda9901e61243e8c03c32ea6589ecac17
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2025-06-16 00:43:15 -0300

    refactor(service): Add comments describing the business rules for calculating discounts

--- src/main/java/com/studies/service/TickerPriceService.java
+++ src/main/java/com/studies/service/TickerPriceService.java
@@ -5,6 +5,16 @@ import com.studies.Passenger;
 import com.studies.PassengerType;
 import com.studies.exception.InvalidPassengerTypeException;
 
+/***
+ * * This service calculates the ticket price based on the passenger type and flight price.
+ *
+ * * - For GOLD passengers:
+ * *   - If the flight price is above 500.0, a 15% discount is applied.
+ * *   - Otherwise, a 10% discount is applied.
+ * * - For SILVER passengers:
+ * *   - If the flight price is above 700.0, a 10% discount is applied.
+ * *   - Otherwise, a 6% discount is applied.
+ */
 public class TickerPriceService {
     public double calculate(Passenger passenger, Flight flight) {
         if(passenger.getType().equals(PassengerType.GOLD)){
