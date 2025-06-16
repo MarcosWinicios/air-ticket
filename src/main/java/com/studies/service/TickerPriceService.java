71696444e1ceaf35b7150bcf1446fcf39917a114
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2025-06-16 00:09:48 -0300

    feat(service): Create method to calculate gold type passengers discount

--- src/main/java/com/studies/service/TickerPriceService.java
+++ src/main/java/com/studies/service/TickerPriceService.java
@@ -2,9 +2,13 @@ package com.studies.service;
 
 import com.studies.Flight;
 import com.studies.Passenger;
+import com.studies.PassengerType;
 
 public class TickerPriceService {
     public double calculate(Passenger passenger, Flight flight) {
-        return 0;
+        if(passenger.getType().equals(PassengerType.GOLD)){
+            return flight.getPrice() * 0.9; // 10% discount for GOLD passengers
+        }
+        return 0.0;
     }
 }
